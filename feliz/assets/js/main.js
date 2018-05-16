8d1e9afb5bb724ea536e60ede97e6fe99412f979
vomaingoc <vomaingoc@gmail.com>
2018-05-16 22:47:10 +0700

    chinh sua

--- feliz/assets/js/main.js
+++ feliz/assets/js/main.js
@@ -1,12 +1,29 @@
 
 $( document ).ready(function() {
-    $('.owl-carousel').owlCarousel({
+    $('.slide-01').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
         responsive:{
             0:{
-                items:1
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+    $('.slide-02').owlCarousel({
+        loop:true,
+        margin:16,
+        nav:true,
+        responsive:{
+            0:{
+                items:2
             },
             600:{
                 items:3
