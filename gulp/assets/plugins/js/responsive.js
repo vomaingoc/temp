171db2562f1822e01fa7ad4ceee82885a27ba5d3
vomaingoc <vomaingoc@gmail.com>
2018-10-23 18:04:44 +0700

    update css

--- gulp/assets/plugins/js/responsive.js
+++ gulp/assets/plugins/js/responsive.js
@@ -1,9 +1,9 @@
 function getdevice() {
+    var htmlWidth = document.getElementsByTagName("html")[0].offsetWidth;
     var md = new MobileDetect(window.navigator.userAgent);
     var myhtml = document.getElementsByTagName("html");
     //Desktop
-    if (md.phone() === null && md.tablet() === null) {
-        //$('html').addClass('desktop').removeClass('mobile').removeClass('tablet').removeClass('phone');
+    if (md.phone() === null && md.tablet() === null && htmlWidth > 1025) {
         myhtml[0].classList.add("isDesktop");
         myhtml[0].classList.remove("isMobile");
         myhtml[0].classList.remove("isTablet");
@@ -11,23 +11,17 @@ function getdevice() {
     }
     //Mobile
     else {
-        //$('html').addClass('mobile').removeClass('desktop');
         myhtml[0].classList.add("isMobile");
         myhtml[0].classList.remove("isDesktop");
         if (md.phone()){
-            //$('html').addClass('phone').removeClass('tablet');
             myhtml[0].classList.add("isPhone");
             myhtml[0].classList.remove("isTablet");
         }
-
         else{
-            //$('html').addClass('tablet').removeClass('phone');
             myhtml[0].classList.add("isTablet");
             myhtml[0].classList.remove("isPhone");
         }
-
     }
-
     //browser
     var ua = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
     var browser = '';
@@ -38,16 +32,12 @@ function getdevice() {
         browser = ua[1].toLowerCase();
         if (browser === 'safari')
         {
-            //$('html').addClass('safari');
             myhtml[0].classList.add("safari");
         }
-
         else{
-            //$('html').removeClass('safari');
             myhtml[0].classList.remove("safari");
         }
     }
 }
-
 document.addEventListener("DOMContentLoaded", getdevice);
-window.addEventListener("resize", getdevice); 
+window.addEventListener("resize", getdevice);
